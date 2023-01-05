f7910c4cd23c75df6d3c555ab81ad2ae6924e186
Souls <52910731+Souls362@users.noreply.github.com>
2023-01-05 15:53:13 +0800

    Fix Error

--- gym_gomoku/envs/gomoku.py
+++ gym_gomoku/envs/gomoku.py
@@ -18,32 +18,32 @@ from gym_gomoku.envs.util import make_expert_policy
 # Black plays first if white did not win in the previous game, and players alternate in placing a stone of their color on an empty intersection.
 
 class GomokuState(object):
-    '''
+    """
     Similar to Go game, Gomoku state consists of a current player and a board.
     Actions are exposed as integers in [0, num_actions), which is to place stone on empty intersection
-    '''
+    """
     def __init__(self, board, color):
-        '''
+        """
         Args:
             board: current board
             color: color of current player
-        '''
+        """
         assert color in ['black', 'white'], 'Invalid player color'
         self.board, self.color = board, color
     
     def act(self, action):
-        '''
+        """
         Executes an action for the current player
-        
         Returns:
             a new GomokuState with the new board and the player switched
-        '''
+        """
         return GomokuState(self.board.play(action, self.color), gomoku_util.other_color(self.color))
     
     def __repr__(self):
-        '''stream of board shape output'''
+        """stream of board shape output"""
         # To Do: Output shape * * * o o
         return 'To play: {}\n{}'.format(six.u(self.color), self.board.__repr__())
+
 
 # Sampling without replacement Wrapper 
 # sample() method will only sample from valid spaces
@@ -51,10 +51,15 @@ class DiscreteWrapper(spaces.Discrete):
     def __init__(self, n):
         self.n = n
         self.valid_spaces = list(range(n))
-    
+
+    @property
+    def shape(self):
+        return ()
+
     def sample(self):
-        '''Only sample from the remaining valid spaces
-        '''
+        """
+        Only sample from the remaining valid spaces
+        """
         if len(self.valid_spaces) == 0:
             print ("Space is empty")
             return None
@@ -63,8 +68,9 @@ class DiscreteWrapper(spaces.Discrete):
         return self.valid_spaces[randint]
     
     def remove(self, s):
-        '''Remove space s from the valid spaces
-        '''
+        """
+        Remove space s from the valid spaces
+        """
         if s is None:
             return
         if s in self.valid_spaces:
@@ -75,10 +81,10 @@ class DiscreteWrapper(spaces.Discrete):
 
 ### Environment
 class GomokuEnv(gym.Env):
-    '''
+    """
     GomokuEnv environment. Play against a fixed opponent.
-    '''
-    metadata = {"render.modes": ["human", "ansi"]}
+    """
+    metadata = {"render_modes": ["human", "ansi"]}
     
     def __init__(self, player_color, opponent, board_size):
         """
@@ -110,7 +116,7 @@ class GomokuEnv(gym.Env):
         self.state = None
         
         # reset the board during initialization
-        self._reset()
+        self.reset()
     
     def _seed(self, seed=None):
         self.np_random, seed1 = seeding.np_random(seed)
@@ -118,7 +124,7 @@ class GomokuEnv(gym.Env):
         seed2 = seeding.hash_seed(seed1 + 1) % 2**32
         return [seed1, seed2]
     
-    def _reset(self):
+    def reset(self):
         self.state = GomokuState(Board(self.board_size), gomoku_util.BLACK) # Black Plays First
         self._reset_opponent(self.state.board) # (re-initialize) the opponent,
         self.moves = []
@@ -142,15 +148,15 @@ class GomokuEnv(gym.Env):
         self.opponent_policy = None
         self.state = None
     
-    def _render(self, mode="human", close=False):
+    def render(self, mode="human", close=False):
         if close:
             return
         outfile = StringIO() if mode == 'ansi' else sys.stdout
         outfile.write(repr(self.state) + '\n')
         return outfile
     
-    def _step(self, action):
-        '''
+    def step(self, action):
+        """
         Args: 
             action: int
         Return: 
@@ -160,7 +166,7 @@ class GomokuEnv(gym.Env):
             info: state dict
         Raise:
             Illegal Move action, basically the position on board is not empty
-        '''
+        """
         assert self.state.color == self.player_color # it's the player's turn
         
         # If already terminal, then don't do anything
@@ -226,10 +232,11 @@ class GomokuEnv(gym.Env):
         else:
             raise error.Error('Unrecognized opponent policy {}'.format(self.opponent))
 
+
 class Board(object):
-    '''
+    """
     Basic Implementation of a Go Board, natural action are int [0,board_size**2)
-    '''
+    """
     
     def __init__(self, board_size):
         self.size = board_size
@@ -249,30 +256,33 @@ class Board(object):
         return coord
     
     def get_legal_move(self):
-        ''' Get all the next legal move, namely empty space that you can place your 'color' stone
-            Return: Coordinate of all the empty space, [(x1, y1), (x2, y2), ...]
-        '''
+        """
+        Get all the next legal move, namely empty space that you can place your 'color' stone
+        Return: Coordinate of all the empty space, [(x1, y1), (x2, y2), ...]
+        """
+
         legal_move = []
         for i in range(self.size):
             for j in range(self.size):
-                if (self.board_state[i][j] == 0):
+                if self.board_state[i][j] == 0:
                     legal_move.append((i, j))
         return legal_move
-    
+
     def get_legal_action(self):
-        ''' Get all the next legal action, namely empty space that you can place your 'color' stone
-            Return: Coordinate of all the empty space, [(x1, y1), (x2, y2), ...]
-        '''
+        """
+        Get all the next legal action, namely empty space that you can place your 'color' stone
+        Return: Coordinate of all the empty space, [(x1, y1), (x2, y2), ...]
+        """
         legal_action = []
         for i in range(self.size):
             for j in range(self.size):
-                if (self.board_state[i][j] == 0):
+                if self.board_state[i][j] == 0:
                     legal_action.append(self.coord_to_action(i, j))
         return legal_action
-    
+
     def copy(self, board_state):
-        '''update board_state of current board values from input 2D list
-        '''
+        """update board_state of current board values from input 2D list
+        """
         input_size_x = len(board_state)
         input_size_y = len(board_state[0])
         assert input_size_x == input_size_y, 'input board_state two axises size mismatch'
@@ -280,27 +290,27 @@ class Board(object):
         for i in range(self.size):
             for j in range(self.size):
                 self.board_state[i][j] = board_state[i][j]
-    
+
     def play(self, action, color):
-        '''
-            Args: input action, current player color
-            Return: new copy of board object
-        '''
+        """
+        Args: input action, current player color
+        Return: new copy of board object
+        """
         b = Board(self.size)
         b.copy(self.board_state) # create a board copy of current board_state
         b.move = self.move
-        
+
         coord = self.action_to_coord(action)
         # check if it's legal move
         if (b.board_state[coord[0]][coord[1]] != 0): # the action coordinate is not empty
             raise error.Error("Action is illegal, position [%d, %d] on board is not empty" % ((coord[0]+1),(coord[1]+1)))
-        
+
         b.board_state[coord[0]][coord[1]] = gomoku_util.color_dict[color]
         b.move += 1 # move counter add 1
         b.last_coord = coord # save last coordinate
         b.last_action = action
         return b
-    
+
     def is_terminal(self):
         exist, color = gomoku_util.check_five_in_row(self.board_state)
         is_full = gomoku_util.check_board_full(self.board_state)
@@ -308,31 +318,33 @@ class Board(object):
             return True
         else:
             return exist
-    
+
     def __repr__(self):
-        ''' representation of the board class
-            print out board_state
-        '''
+        """
+        representation of the board class
+        print out board_state
+        """
+
         out = ""
         size = len(self.board_state)
-        
+
         letters = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')[:size]
         numbers = list(range(1, 100))[:size]
-        
+
         label_move = "Move: " + str(self.move) + "\n"
         label_letters = "     " + " ".join(letters) + "\n"
         label_boundry = "   " + "+-" + "".join(["-"] * (2 * size)) + "+" + "\n"
-        
+
         # construct the board output
         out += (label_move + label_letters + label_boundry)
-        
+
         for i in range(size-1,-1,-1):
             line = ""
             line += (str("%2d" % (i+1)) + " |" + " ")
             for j in range(size):
                 # check if it's the last move
                 line += gomoku_util.color_shape[self.board_state[i][j]]
-                if (i,j) == self.last_coord:
+                if (i, j) == self.last_coord:
                     line += ")"
                 else:
                     line += " "
@@ -340,7 +352,7 @@ class Board(object):
             out += line
         out += (label_boundry + label_letters)
         return out
-    
+
     def encode(self):
         '''Return: np array
             np.array(board_size, board_size): state observation of the board
